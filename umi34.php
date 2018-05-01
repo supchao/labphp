bed531b67106c54d02bd3e4897cba19be45c12ea
supchao <jh86078@gmail.com>
2018-05-01 16:15:45 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	hw3.php

--- umi34.php
+++ umi34.php
@@ -1,14 +1,18 @@
 
 <?php
-$fp = fopen("test1/iii.txt",'r');
+$fp = fopen("test1/iii2.txt",'a+');
 
 
-while($c = fgets($fp)){////     fgetc 1byte讀取  //fgets整列讀取    //fgetss去除html 標籤
-    echo $c . '<br>';
-}
+//while($c = fgets($fp)){////     fgetc 1byte讀取  //fgets整列讀取    //fgetss去除html 標籤
+//    echo $c . '<br>';
+//}
+
+
+fwrite($fp,"hello, umi");
 
 //$cont = fread($fp,4096);
-//$cont = fgetc($fp);
 //echo $cont;
+
+
 fclose($fp);
 ?>
---
+++ umi37.php
@@ -0,0 +1,8 @@
+<?php  ///  39  40
+$filename =$_REQUEST['filename'];
+$cont = $_REQUEST['cont'];
+$fp = fopen("test3/{$filename}",'w');
+fwrite($fp,$cont);
+fclose($fp);
+header("Location : test3/{$filename}");
+?>
