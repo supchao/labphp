7c7f98de96d3c4b1af67cd368202a73f50727f6f
supchao <jh86078@gmail.com>
2018-05-03 08:46:23 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	hw3.php

---
+++ umi40.php
@@ -0,0 +1,10 @@
+
+<form method="post"
+      enctype="multipart/form-data"
+      action="umi41.php">
+    <input type="file" name="upload[]"/><br>
+    <input type="file" name="upload[]"/><br>
+    <input type="file" name="upload[]"/><br>
+    <input type="file" name="upload[]"/><br>
+    <input type="submit" value="Upload"/><br>
+</form>
